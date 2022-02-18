b3a8db17d07635fe691cc467d9a5a1b675e824fe
adidas420x <58811906+adidas420x@users.noreply.github.com>
2022-02-18 14:19:42 +0100

    Lavet foldere som inkluderer API, Webclient og Desktopclient inkl deres projekter i form af model og data

---
+++ Sem3Projekt/WebClient.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebClient.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
