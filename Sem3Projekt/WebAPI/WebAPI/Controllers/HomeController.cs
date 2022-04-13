088fc6c193b7fa24b1cf6ff93fad72ba41adc8c8
NichumUCN <1087651@ucn.dk>
2022-04-13 12:40:16 +0200

    Indentity

---
+++ Sem3Projekt/WebAPI/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers {
+	public class HomeController : Controller {
+		// GET: HomeController
+		[Route("[controller]")]
+		public ActionResult Index() {
+			return View();
+		}
+
+		// GET: HomeController/Details/5
+		[Route("privacy")]
+		public ActionResult Privacy() {
+			return View();
+		}
+		
+	}
+}
--- Sem3Projekt/WebAPI/WebAPI/databaseScript.sql
+++ Sem3Projekt/WebAPI/WebAPI/databaseScript.sql
@@ -9,3 +9,22 @@ CREATE Table Person
 	BirthDate date,
 	Email nvarchar(256) primary key not null
 );
+
+
+/*
+Create with AspNetUsers Normalized Email primary key on Person
+use [3.SemesterDb]
+
+ALTER TABLE ASPNetUsers ADD UNIQUE (NormalizedEmail);
+
+CREATE Table Person 
+(
+	FirstName varchar(255) not null,
+	LastName varchar(255) not null,
+	NickName varchar(255) not null,
+	BirthDate date,
+	Email nvarchar(256) PRIMARY KEY,
+	FOREIGN KEY (Email) REFERENCES AspNetUsers(NormalizedEmail)
+);
+
+*/
