f60570bb061c7ef6006c431312336cffd6236dbe
Sundebergius <1089441@ucn.dk>
2022-04-21 15:25:16 +0200

    Scaffolding for the entire desktop client, working with getting users into panels of the application. Service, PersonService, to recieve persons from the API. PersonController.

---
+++ Sem3Projekt/DesktopConsumer/DesktopConsumer/Controller/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using DesktopConsumer.Models;
+using DesktopConsumer.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopConsumer.Controller
+{
+    public class PersonController
+    {
+        private PersonService personService;
+
+        public PersonController()
+        {
+            personService = new PersonService();  
+        }
+
+        public async Task<Person> GetPersonByEmail(string email)
+        {
+           return await personService.GetPersonByEmail(email);
+        }
+    }
+}
