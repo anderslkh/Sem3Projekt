4f2de63ad3626e14112eb074da259d07bb762607
Oliver <1089446@ucn.dk>
2022-04-25 15:10:54 +0200

    made http endpoint in API controller

---
+++ Sem3Projekt/WebConsumer/WebConsumer/Controllers/TournamentsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebConsumer.Controllers {
+    public class TournamentsController : Controller {
+        // GET: TournamentsController
+        public ActionResult Index() {
+            return View();
+        }
+
+        // GET: TournamentsController/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: TournamentsController/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: TournamentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: TournamentsController/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: TournamentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: TournamentsController/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: TournamentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
