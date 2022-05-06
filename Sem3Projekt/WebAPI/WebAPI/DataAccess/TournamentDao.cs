4c2eea8c75cd1094cc0b0195bf31d1810b0ada5a
NichumUCN <1087651@ucn.dk>
2022-05-06 12:30:35 +0200

    Made solution for concurrerncy issue

--- Sem3Projekt/WebAPI/WebAPI/DataAccess/TournamentDao.cs
+++ Sem3Projekt/WebAPI/WebAPI/DataAccess/TournamentDao.cs
@@ -15,14 +15,16 @@ namespace WebAPI.DataAccess {
 
 		public Tournament GetItemById(int tournamentId) {
 			Tournament foundTournament = null;
-			string sqlQuery =
+			string sqlQueryAdmin =
 				"SELECT TournamentId, TournamentName, TimeOfEvent, RegistrationDeadline, MinParticipants, MaxParticipants, PersonEmail FROM TournamentInfoView WHERE TournamentId = @TournamentId";
+			string sqlQueryUser = 
+				"SELECT TournamentId, TournamentName, TimeOfEvent, RegistrationDeadline, MinParticipants, MaxParticipants, EnrolledParticipants FROM TournamentInfo WHERE TournamentId = @TournamentId";
 			var param = new { TournamentId = tournamentId };
 			
 			using (_conn)
 			{
 				var tournaments = _conn.Query<Tournament, string, Tournament>(
-					sqlQuery, (tournament, personEmail) =>
+					sqlQueryAdmin, (tournament, personEmail) =>
 					{
 						tournament.ListOfParticipantIds.Add(personEmail);
 						return tournament;
@@ -41,15 +43,27 @@ namespace WebAPI.DataAccess {
 
 		public int EnrollInTournament(int tournamentId, int enrolledParticipants, string personEmail) {
 			int result = -1;
-			string sqlQuery =
+			string sqlQueryCheckAvailable = "SELECT ISNULL((SELECT 1 FROM TournamentInfo WHERE TournamentInfo.TournamentId = @TournamentId AND (TournamentInfo.EnrolledParticipants = @EnrolledParticipants)), 0)";
+			string sqlQueryInsert =
 				"INSERT INTO PersonInTournament (PersonEmail, TournamentId) SELECT * FROM (SELECT @PersonEmail AS PersonEmail, @TournamentId AS TournamentId) AS temp WHERE NOT EXISTS (SELECT @PersonEmail FROM PersonInTournament WHERE PersonEmail = @PersonEmail AND TournamentId = @TournamentId)";
-			
+			var checkParam = new
+			{
+				TournamentId = tournamentId,
+				EnrolledParticipants = enrolledParticipants
+			};
+
 			var param = new {
 				PersonEmail = personEmail,
-				TournamentId = tournamentId
+				TournamentId = tournamentId,
 			};
-			using (_conn) {
-				result = _conn.Execute(sqlQuery, param);
+			using (_conn)
+			{
+				
+				if ((int)_conn.ExecuteScalar(sqlQueryCheckAvailable, checkParam) == 1)
+				{
+					result = _conn.Execute(sqlQueryInsert, param);
+				}
+				
 			}
 
 			return result;
